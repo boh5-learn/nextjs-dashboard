bacd24b90a9ccc84b7262b80a43b1b4dc2bede57
boh5 <bohuang1994@gmail.com>
2024-01-06 17:23:09 +0800

    :sparkles: Chapter 9 - Streaming

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,45 +1,48 @@
-import {
-  BanknotesIcon,
-  ClockIcon,
-  UserGroupIcon,
-  InboxIcon,
-} from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import {BanknotesIcon, ClockIcon, InboxIcon, UserGroupIcon} from '@heroicons/react/24/outline'
+import {lusitana} from '@/app/ui/fonts'
+import {fetchCardData} from '@/app/lib/data'
 
 const iconMap = {
   collected: BanknotesIcon,
   customers: UserGroupIcon,
   pending: ClockIcon,
   invoices: InboxIcon,
-};
+}
 
 export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData()
+
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
-  );
+  )
 }
 
 export function Card({
-  title,
-  value,
-  type,
-}: {
+                       title,
+                       value,
+                       type,
+                     }: {
   title: string;
   value: number | string;
   type: 'invoices' | 'customers' | 'pending' | 'collected';
 }) {
-  const Icon = iconMap[type];
+  const Icon = iconMap[type]
 
   return (
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
@@ -54,5 +57,5 @@ export function Card({
         {value}
       </p>
     </div>
-  );
+  )
 }
